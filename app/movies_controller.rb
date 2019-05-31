f6fba45978dcc1bd275b20c0782e3117ace45405
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-31 20:56:18 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -32,8 +32,8 @@ def can_be_created_in_a_block(args={title: "Home Alone", release_date: 1990})
   # release_date = 1990
   
   Movie.create do |m|
-  m.title = "The Room"
-  m.release_date = 2003
+    m.title = args[:title]
+    m.release_date = args[:release_date]
    
   end
 end
